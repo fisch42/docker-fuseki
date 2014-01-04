e9192ea9657c3d36bee2538622fed1c5ba6719da
fisch42 <fionaschmidtke@web.de>
2014-01-04 14:35:14 +0100

    Update download-fuseki.sh

--- script/download-fuseki.sh
+++ script/download-fuseki.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 distr=`curl -silent https://www.apache.org/dist/jena/binaries/ | perl -ne '/href="(jena-fuseki-\d\.\d\.\d)-distribution.tar.gz"/ && print $1'`
 wget --quiet https://www.apache.org/dist/jena/binaries/$distr-distribution.tar.gz -O- | tar -zx --strip-components 1 -C fuseki $distr
-export FUSEKI_HOME=/opt/fuseki/
